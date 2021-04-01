6f37ce1acdb3cf170335e536e01cd004a792bd57
samuelamos40 <81754216+samuelamos40@users.noreply.github.com>
2021-04-01 13:41:18 +0100

    Add files via upload

---
+++ sammy.py
@@ -0,0 +1,32 @@
+#Question 1
+	def personal_details():
+	    name = "Sunday Samuel Amos "
+	    department = "Mathematical Science"
+	    print("Name: {}\nDepartment: {}".format(name, department))
+	
+	personal_details()
+	#Question 2
+	scare = "Boo!"
+	print(scare)
+	# Question 3
+	scare_var1 = 5
+	scare_var2 = 5
+	result = scare_var1 + scare_var2
+	scare_var3 = result
+	print(scare_var1,scare_var2,scare_var3)
+	
+	#Question 4
+	variablee = 12/0.5
+	print(variablee)
+	#Question 5
+	x = 25
+	y = 30
+	sum = x + y
+	print(sum)
+	#Question 6
+	sci = 8 % 3
+	print(sci)
+	#Question 7
+	x = (2 * 4) * (4 + 2)
+	print(x)
+
